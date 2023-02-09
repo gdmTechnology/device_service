c0f6b556f7e0c1c224677a03f599bddc19b4671e
guiac <gui.acassemiro@gmail.com>
2023-02-08 22:34:43 -0300

    :test_tube: UpdateDeviceRepository

--- tests/infra/db/device.mongo.repository.spec.ts
+++ tests/infra/db/device.mongo.repository.spec.ts
@@ -20,9 +20,29 @@ describe('DeviceMongoRepository', () => {
     afterEach(async () => await MongoTestDbHelper.clearDatabase())
     afterAll(async () => await MongoTestDbHelper.disconnect())
 
-    test('Should create a device on success', async () => {
-        const sut = makeSut()
-        const device = await sut.save(createDeviceParams())
-        expect(device).toBeTruthy()
+    describe('CreateDeviceRepository', () => {
+        test('Should create a device on success', async () => {
+            const sut = makeSut()
+            const device = await sut.save(createDeviceParams())
+            expect(device).toBeTruthy()
+        })
+    })
+
+    describe('UpdateDeviceRepository', () => {
+        test('Should update a device on success', async () => {
+            const sut = makeSut()
+            const req = createDeviceParams()
+            await sut.save(createDeviceParams())
+            const updatedDvc = await sut.update({ ...req, deviceName: 'teste' })
+            expect(updatedDvc.deviceName).toBe('teste')
+        })
+
+        test('Should return null if device was not found ', async () => {
+            const sut = makeSut()
+            const req = createDeviceParams()
+            await sut.save(createDeviceParams())
+            const updatedDvc = await sut.update({ ...req, deviceIdentification: 'invalid' })
+            expect(updatedDvc).toBeNull()
+        })
     })
 })
